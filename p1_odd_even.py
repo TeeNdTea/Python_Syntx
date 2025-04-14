37146dd47804f2fc51fd580897daa41caaaefcb3
Tee Faizah <tee.faizah@gmail.com>
2025-04-14 20:25:51 +0600

    Add files via upload

---
+++ p1_odd_even.py
@@ -0,0 +1,18 @@
+# if a given number is odd/even
+
+# for any integer whole number
+num=int(input("Enter a number: "))
+
+if num%2==0:
+    print("EVEN")
+else:
+    print("ODD")
+
+
+#float
+# num=float(input("Enter a number:"))
+# new=int(num)
+# if new%2==0:
+#     print("even")
+# else:
+#     print("odd")
