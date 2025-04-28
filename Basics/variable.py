1c3943662594c8a3de53f3a79422d756fffb8f58
Tee Faizah <tee.faizah@gmail.com>
2025-04-28 21:37:37 +0600

    Add files via upload

---
+++ Basics/variable.py
@@ -0,0 +1,13 @@
+#Variables
+'''variable naming:
+Camel Case: "myVariableName" - Starts with a lowercase letter and capitalizes the first letter of each subsequent word. 
+Snake Case: "my_variable_name" - Separates words with underscores, all lowercase. 
+Pascal Case: "MyVariableName" - Capitalizes the first letter of every word. 
+Kebab Case: "my-variable-name" - Separates words with hyphens. (Not used in Python) '''
+
+name="Lisa"
+age=20 # float & int can be added with ,
+print("Our new student is" +" "+ name) #concatanation...only strings can be add
+print(name+ " "+"is",age, "years old") # space can be given by " "
+print(name+" "+ "lives in Bengalore")
+print("We have a similar aged student who is",age)
